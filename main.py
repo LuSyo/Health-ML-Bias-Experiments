save results from main; stratified perf metrics; causal model validation: CCA; fix args type in parser

main.py
@@ -1,5 +1,6 @@
-import torch
 import pandas as pd
+import numpy as np
+import os
 from src.config import Config
 from src.data_loader import make_bucketed_loader
 from src.model import DCEVAE
@@ -7,9 +8,13 @@ from src.train import train_dcevae
 from src.test import test_dcevae
 from src.utils import parse_args, load_feature_mapping, setup_logger
 from src.plots import train_val_loss_curve, disc_tc_loss_curve, distillation_loss_curve
+from src.metrics import get_cca
 
 def main():
   args = parse_args()
+
+  results_path = f'{args.root_dir}{Config.RESULTS_DIR}{args.exp_name}'
+  os.makedirs(results_path, exist_ok=True)
 
   # Initialise logger
   logger = setup_logger(Config.LOG_DIR, args.exp_name)
@@ -65,11 +70,23 @@ def main():
 
     training_metrics = pd.DataFrame(training_log)
 
-    train_val_loss_curve(training_metrics, args)
-    disc_tc_loss_curve(training_metrics, args)
-    distillation_loss_curve(training_metrics, args)
+    train_val_loss_fig = train_val_loss_curve(training_metrics, args)
+    train_val_loss_fig.savefig(f'{results_path}/train_val_loss_curve.png')
+    disc_tc_loss_fig = disc_tc_loss_curve(training_metrics, args)
+    disc_tc_loss_fig.savefig(f'{results_path}/disc_tc_loss_curve.png')
+    distillation_loss_fig = distillation_loss_curve(training_metrics, args)
+    distillation_loss_fig.savefig(f'{results_path}/distillation_loss_curve.png')
 
-    test_dcevae(model, test_loader, logger, args)
+    test_results, perf_metrics, strat_perf_metrics = test_dcevae(model, test_loader, logger, args)
+
+    strat_perf_metrics.to_markdown(f'{results_path}/stratified_perf_metrics.txt', index=False)
+
+    # CAUSAL MODEL VALIDATION
+    # Independence of u_desc and u_corr
+    u_desc_matrix = np.stack(test_results['u_desc'].values)
+    u_corr_matrix = np.stack(test_results['u_corr'].values)
+    u_u_cca = get_cca(u_desc_matrix, u_corr_matrix)
+    logger.info(f'Canonical Correlation Analysis between U_corr and U_desc: {u_u_cca}')
 
   except Exception as e:
     logger.error(f'Experiment failed: {str(e)}', exc_info=True)

src/test.py
@@ -1,14 +1,15 @@
-from sklearn.metrics import accuracy_score, roc_auc_score, confusion_matrix
 import torch
 import numpy as np
 import pandas as pd
-from src.metrics import calculate_performance_metrics
+from src.metrics import calculate_performance_metrics, stratified_perf
 
 def test_dcevae(model, test_loader, logger, args):
+
   device = args.device
   model.eval()
 
-  all_y_true, all_y_pred_prob, all_y_cf_prob, all_sens = [], [], [], []
+  all_y_true, all_y_pred_prob, all_y_cf_prob, all_sens, all_u_corr, all_u_desc = \
+    [], [], [], [],[], []
 
   with torch.no_grad():
     for batch in test_loader:
@@ -28,12 +29,16 @@ def test_dcevae(model, test_loader, logger, args):
       all_y_pred_prob.append(torch.sigmoid(y_pred_prob).cpu().numpy())
       all_y_cf_prob.append(torch.sigmoid(y_cf_prob).cpu().numpy())
       all_sens.append(x_sens.cpu().numpy())
-
+      all_u_corr.append(u_corr.cpu().numpy())
+      all_u_desc.append(u_desc.cpu().numpy())
+  
   test_results = pd.DataFrame({
       'y_true': np.concatenate(all_y_true).flatten(),
       'y_pred_prob': np.concatenate(all_y_pred_prob).flatten(),
       'y_cf_prob': np.concatenate(all_y_cf_prob).flatten(),
-      'sens': np.concatenate(all_sens).flatten()
+      'sens': np.concatenate(all_sens).flatten(),
+      'u_corr': list(np.concatenate(all_u_corr)),
+      'u_desc': list(np.concatenate(all_u_desc)),
   })
   test_results['y_pred'] = (test_results['y_pred_prob'] > 0.5).astype(int)
 
@@ -41,10 +46,18 @@ def test_dcevae(model, test_loader, logger, args):
     test_results['y_true'],
     test_results['y_pred'],
     test_results['y_pred_prob'])
+  
+  strat_perf_metrics = stratified_perf(
+    test_results['y_true'],
+    test_results['y_pred'],
+    test_results['y_pred_prob'],
+    test_results['sens'],
+  )
 
   logger.info(f'Test Accuracy: {perf_metrics['accuracy']:.4f}')
   logger.info(f'Test AUC: {perf_metrics['roc_auc']:.4f}')
+  logger.info(f'Test Brier Score: {perf_metrics['brier_score']:.4f}')
   logger.info(f'Test False Negative Rate: {perf_metrics['fnr']:.4f}')
   logger.info(f'Test False Positive Rate: {perf_metrics['fpr']:.4f}')
 
-  return test_results
+  return test_results, perf_metrics, strat_perf_metrics

src/train.py
@@ -1,6 +1,7 @@
 import numpy as np
 import torch
 import torch.optim as optim
+import os
 from src.config import Config
 
 def train_dcevae(model, train_loader, val_loader, logger, args):
@@ -94,14 +95,15 @@ def train_dcevae(model, train_loader, val_loader, logger, args):
     logger.info(f'Avg VAE Train Loss: {avg_train_loss}')
     logger.info(f'Avg VAE Validation Loss: {avg_val_loss}')
   
-  model_path = f'{args.root_dir}{Config.MODELS_DIR}{args.exp_name}_dcevae.pth'
+  model_path = f'{args.root_dir}{Config.MODELS_DIR}'
+  os.makedirs(model_path, exist_ok=True)
   torch.save({
     'epoch': args.n_epochs,
     'model_state_dict': model.state_dict(),
     'optimizer_main_state_dict': main_optimiser.state_dict(),
     'discrim_optim_state_dict': discrim_optimiser.state_dict(),
     'args': args
-  }, model_path)
+  }, f'{model_path}{args.exp_name}_dcevae.pth')
   logger.info(f'DCEVAE model saved to {model_path}')
   
   return training_log, epoch_metrics_log