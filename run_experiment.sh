6fbc649dba65f54f1dd927f87cd71c60193a4b7d
Ludivine Siau <ludivinesiau@gmail.com>
2026-03-06 19:37:06 +0000

    experiment bash script

--- run_experiment.sh
+++ run_experiment.sh
@@ -1,5 +1,16 @@
 python3 main.py \
-  --exp_name "24-Feb_80" \
+  --exp_name "6-Mar_0" \
   --data "heart_disease_cleaned.csv" \
   --mapping "configs/uci_feature_mapping.json" \
-  --n_epochs 80
+  --n_epochs 100 \
+  --vae_lr 0.001 \
+  --disc_lr 0.0005 \
+  --pred_a 5 \
+  --tc_b 15 \
+  --fair_b 1 \
+  --kl_warm_up 0 \
+  --tc_warm_up 0 \
+  --distill_warm_up 0 \
+  --ud_dim 3 \
+  --uc_dim 3
+
